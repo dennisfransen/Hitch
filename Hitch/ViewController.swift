06ef8df2c7ac6ee6f4a32d6abc1c21602467f5ab
Dennis Fransen <dennisfransen@denniss-mbp.home>
2019-03-26 09:56:57 +0100

    first commit

---
+++ Hitch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hitch
+//
+//  Created by Dennis Fransen on 2019-03-26.
+//  Copyright © 2019 Dennis Fransen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
